57986ba8d7717d2bb8c56d5e308db7d47249185b
VictorBarros <victorbarros1130@gmail.com>
2021-10-16 16:50:08 -0300

    refact image button and initi button handler

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,32 +1,46 @@
 import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Alert } from 'react-native'
 import styles from './App.styles'
 import ImageOption from './src/components/ImageOption'
 import question from './assets/data/oneQuestionWithOption'
 import Button from './src/components/Button'
 
-const { title, options } = question
+const questions = [question]
 console.log('starting')
-
-const onButtonPress = () => console.warn("button")
 
 const App = () => {
   const [selected, setSelected] = useState(undefined)
+  const [question, setQuestion] = useState(questions[0])
+  const { title, options } = question
+
+
+  const onButtonPress = () => {
+    if (selected.correct) {
+      Alert.alert("correct")
+      // TODO: move p next question
+    } else {
+      Alert.alert("wrooong")
+      // TODO: move p next question
+    }
+  }
 
   return (
     <View style={styles.root}>
       <Text style={styles.title}>{title}</Text>
 
       <View style={styles.optionsContainer}>
-        {options.map(({ id, image, text }) => (
-          <ImageOption
-            key={id}
-            image={image}
-            text={text}
-            isSelected={selected === id}
-            onPress={() => setSelected(id)}
-          />
-        ))}
+        {options.map((option) => {
+          const { id, text, image } = option
+          return (
+            <ImageOption
+              key={id}
+              image={image}
+              text={text}
+              isSelected={selected?.id === id}
+              onPress={() => setSelected(option)}
+            />
+          )
+        })}
       </View>
 
       <Button text="Check" onPress={onButtonPress} disabled={!selected} />
